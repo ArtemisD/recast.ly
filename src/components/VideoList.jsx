639a6ef25d3fe384264197c8c9c3453440d5f244
ArtemisD <dianah456@gmail.com>
2017-07-09 19:42:05 -0700

    VideoList & VideoListEntry

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,30 +1,9 @@
-var VideoList = () => (
+var VideoList = (props) => (
   <div className="video-list media">
 
-    <VideoListEntry
-    link={exampleVideoData[0].snippet.thumbnails.medium.url}
-    title={exampleVideoData[0].snippet.title}
-    description={exampleVideoData[0].snippet.description} />
-
-    <VideoListEntry
-    link={exampleVideoData[1].snippet.thumbnails.medium.url}
-    title={exampleVideoData[1].snippet.title}
-    description={exampleVideoData[1].snippet.description} />
-
-    <VideoListEntry
-    link={exampleVideoData[2].snippet.thumbnails.medium.url}
-    title={exampleVideoData[2].snippet.title}
-    description={exampleVideoData[2].snippet.description} />
-
-    <VideoListEntry
-    link={exampleVideoData[3].snippet.thumbnails.medium.url}
-    title={exampleVideoData[3].snippet.title}
-    description={exampleVideoData[3].snippet.description} />
-
-    <VideoListEntry
-    link={exampleVideoData[4].snippet.thumbnails.medium.url}
-    title={exampleVideoData[4].snippet.title}
-    description={exampleVideoData[4].snippet.description} />
+  {props.videos.map(video =>
+    <VideoListEntry video = {video}/>
+  )}
 
   </div>
 );
